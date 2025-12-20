36280cc13866f33106bfa01ef6a35feb67b55dec
Like-Su <1325465198@qq.com>
2025-12-20 15:07:15 +0800

    feat(decimal to binary): decimal to binary

--- TypeScript/__test__/binary/binaryTransformer.spec.ts
+++ TypeScript/__test__/binary/binaryTransformer.spec.ts
@@ -1,4 +1,4 @@
-import { binaryToDecimal } from "@/binary/binaryTransformer";
+import { binaryToDecimal, decimalToBinary, decimalToBinaryByWeight } from "@/binary/binaryTransformer";
 import { describe, expect, it } from "vitest";
 
 describe('binary transformer', () => {
@@ -22,5 +22,35 @@ describe('binary transformer', () => {
     expect(binaryToDecimal(['1', '0', '1', '0'])).toBe(10);
     expect(binaryToDecimal(['1', '0', '1', '1'])).toBe(11);
     expect(binaryToDecimal(['1', '1', '1', '1'])).toBe(15);
+  })
+})
+
+describe('decimal transformer', () => {
+  it('should transform decimal to binary string', () => {
+    expect(decimalToBinary(0)).toBe('0');
+    expect(decimalToBinary(1)).toBe('1');
+    expect(decimalToBinary(2)).toBe('10');
+    expect(decimalToBinary(4)).toBe('100');
+    expect(decimalToBinary(8)).toBe('1000');
+    expect(decimalToBinary(9)).toBe('1001');
+    expect(decimalToBinary(10)).toBe('1010');
+    expect(decimalToBinary(11)).toBe('1011');
+    expect(decimalToBinary(15)).toBe('1111');
+  })
+})
+
+describe('decimal transformer by weight', () => {
+  it('should transform decimal to binary string by weight', () => {
+    const toBinary = decimalToBinaryByWeight(4);
+    expect(toBinary('0001')).toBe(1);
+    expect(toBinary('0010')).toBe(2);
+    expect(toBinary('0100')).toBe(4);
+    expect(toBinary('1000')).toBe(8);
+    expect(toBinary('1001')).toBe(9);
+    expect(toBinary('1010')).toBe(10);
+    expect(toBinary('1011')).toBe(11);
+    expect(toBinary('1111')).toBe(15);
+    expect(toBinary('11111')).toBe(31);
+    expect(toBinary('111111')).toBe(63);
   })
 })
--- TypeScript/binary/binaryTransformer.ts
+++ TypeScript/binary/binaryTransformer.ts
@@ -1,6 +1,7 @@
 import { isNumber, isString } from "@/utils/is";
 
 const BINARY_MAP = "01";
+// 二进制转换十进制
 export function binaryToDecimal(binary: string | Array<string>) {
   let originBinary: string | string[] | number = binary;
   if (isNumber(binary)) {
@@ -9,11 +10,67 @@ export function binaryToDecimal(binary: string | Array<string>) {
   if (isString(binary)) {
     originBinary = binary.split('') as string[];
   }
-  console.log(originBinary)
+
   originBinary = (originBinary as string[]).filter(binary => BINARY_MAP.includes(binary));
 
   return originBinary.reduceRight((prev, cur, index, binarys) => {
     const decimal = Number(cur) === 1 ? Math.pow(2, binarys.length - index - 1) : 0;
     return prev + decimal;
   }, 0);
+}
+
+// 十进制转换二进制
+export function decimalToBinary(decimal: number) {
+  if (decimal === 0) return '0';
+  const binarys = [];
+  while (decimal > 0) {
+    binarys.push(decimal % 2);
+    decimal = Math.floor(decimal / 2);
+  }
+
+  // return binarys.reverse().join('');
+  return binarys.reduceRight((prev, cur) => prev += cur, '');
+}
+
+// 二进制转换十进制 凑权法
+export function decimalToBinaryByWeight(weight: number | 'auto') {
+  const _binaryMap: Record<string, number> = {};
+  // 创建二进制表
+  const createBitMap = (binary: string | number) => {
+    let bits: string | string[] = '';
+    let bitCount = 0;
+    if (typeof binary === 'number') {
+      bits = weight.toString();
+    } else {
+      bits = binary.split('');
+      bitCount = Object.keys(_binaryMap).length;
+    }
+
+    if (_binaryMap[bitCount]) return _binaryMap;
+
+    // patch bit map
+    for (let i = bitCount; i < bits.length; i++) {
+      _binaryMap[i] = Math.pow(2, i);
+    }
+    return _binaryMap;
+  }
+
+  // 是否为 1
+  const isTrue = (binary: string, index: number) => binary[index] === '1';
+
+  // 是否将 二进制表进行扩展
+  const isVExtend = (binary: string) => binary.length > Object.keys(_binaryMap).length;
+
+  return (binary: string) => {
+    const bitMap = isVExtend(binary) ? createBitMap(binary) : _binaryMap;
+    const bitMapValue = Object.values(bitMap);
+    let decimal = 0;
+    binary = binary.padStart(bitMapValue.length, '0');
+    for (let i = 0; i < binary.length; i++) {
+      if (isTrue(binary, i)) {
+        decimal += bitMapValue[bitMapValue.length - 1 - i] as any;
+      }
+    }
+    return decimal;
+  }
 }
